ea22d3d160791d62fbd71c7f29420cbc59ff2a05
Scott Fukuda <sdf72@cornell.edu>
2025-11-12 16:20:20 -0500

    Added recommender interface and began frontend logic

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,21 @@
-let () = print_endline "Hello, World!"
+open Terminal_tok
+type video = {
+  title : string;
+  ascii : string;
+  genre : string;
+}
+
+type user = {
+  name : string;
+  mutable vid_history : video * string * string list;
+      (* feel free to expand here: you can add stuff like (video , watchtime,
+         liked..??)*)
+}
+let run () : 'a Lwt.t =
+  let user = 
+  let%lwt () = Lwt_io.printl "Instructions \n press enter to continue" in
+  let%lwt a = Lwt_io.read_line Lwt_io.stdin in
+  let%lwt ascii = Recommender.recommend user in
+  Lwt.return ()
+
+let _ = Lwt_main.run (run ())

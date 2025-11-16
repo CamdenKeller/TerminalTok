120792b3e8f97046b608bcf8dc11014ae4b09a35
Ruben Lemmon <rcl229@cornell.edu>
2025-11-16 17:50:35 -0500

    Implements a basic version of the offline chrome dinosaur game
     - gives the option for online and offline mode
     - in offline mode, immediately starts chrome dinosaur game

---
+++ bin/dinotok.ml
@@ -0,0 +1,97 @@
+open Lwt.Infix
+
+let () = Random.self_init ()
+let init_distance = 30
+let name = "YOU"
+let fps = 0.05
+let input_flag = ref false
+let until_input = ref 0
+let score = ref 0
+let until_67 = ref init_distance
+
+exception Clarkson of string
+
+let line_num n =
+  match n with
+  | 6 | 2 -> 3
+  | 5 | 3 -> 5
+  | 4 -> 6
+  | _ -> 0
+
+let jump n =
+  let line = line_num n in
+  String.make 5 '\n' ^ "Score: " ^ string_of_int !score
+  ^ String.make (18 - line) '\n'
+  ^ "   " ^ name ^ String.make line '\n'
+  ^ if line = 0 then "" else "   "
+
+(* terminal setup *)
+let original_termios = Unix.tcgetattr Unix.stdin
+
+let enable_raw_mode () =
+  let raw = { original_termios with Unix.c_icanon = false; c_echo = false } in
+  Unix.tcsetattr Unix.stdin Unix.TCSANOW raw
+
+let restore_terminal () =
+  Unix.tcsetattr Unix.stdin Unix.TCSANOW original_termios
+
+let rec print_loop () =
+  Lwt.catch
+    (fun () ->
+      let had_input = !input_flag in
+      input_flag := false;
+
+      let new_67 = !until_67 = init_distance && Random.int 10 = 0 in
+      if new_67 then until_67 := init_distance - 1
+      else if !until_67 <> init_distance then
+        until_67 := if !until_67 = 0 then init_distance else !until_67 - 1;
+
+      score := !score + 1;
+
+      let msg =
+        jump !until_input
+        ^
+        if !until_67 <> init_distance then String.make !until_67 ' ' ^ "67"
+        else ""
+      in
+
+      if (not had_input) && !until_input = 0 then ()
+      else if not had_input then until_input := !until_input - 1
+      else until_input := 6;
+
+      Lwt_io.printl msg >>= fun () ->
+      Lwt_unix.sleep fps >>= fun () ->
+      if !until_67 = 0 && !until_input = 0 then
+        raise (Clarkson "Clarkson is coming for you");
+      print_loop ())
+    (function
+      | Clarkson msg ->
+          Lwt_unix.sleep 1.8 >>= fun () ->
+          restore_terminal ();
+          print_endline msg;
+          exit 0
+      | e ->
+          restore_terminal ();
+          raise e)
+
+let cooldown = ref Lwt.return_unit
+
+let rec input_loop () =
+  !cooldown >>= fun () ->
+  Lwt_io.read_char_opt Lwt_io.stdin >>= function
+  | None -> input_loop ()
+  | Some '\n' | Some '\r' ->
+      input_flag := true;
+      cooldown := Lwt_unix.sleep (6.0 *. fps);
+      input_loop ()
+  | Some _ -> input_loop ()
+
+(* --------- WRAPPED AS A FUNCTION --------- *)
+
+let run_dino () : unit Lwt.t =
+  enable_raw_mode ();
+  Lwt.finalize
+    (fun () -> Lwt.join [ print_loop (); input_loop () ])
+    (fun () ->
+      restore_terminal ();
+      Lwt.return_unit)
--- bin/main.ml
+++ bin/main.ml
@@ -24,9 +24,9 @@ let play_ascii_video (file : string) : unit Lwt.t =
 let load_video_list (folder : string) : string list =
   Sys.readdir folder |> Array.to_list
   |> List.filter (fun f ->
-      Filename.check_suffix f ".mp4"
-      || Filename.check_suffix f ".mov"
-      || Filename.check_suffix f ".mkv")
+         Filename.check_suffix f ".mp4"
+         || Filename.check_suffix f ".mov"
+         || Filename.check_suffix f ".mkv")
   |> List.map (Filename.concat folder)
 
 (** [run ()] starts the TerminalTok session *)
@@ -42,81 +42,91 @@ let run () : unit Lwt.t =
        |  - Enter anything else to go to the next Tok\n\n\
        (press ENTER to begin session)"
   in
-
-  let%lwt () =
-    Lwt_io.printl "Select playback mode: (1) Normal ASCII (2) Video ASCII"
-  in
+  let%lwt () = Lwt_io.printl "Select mode: (1) Online (2) Offline" in
   let%lwt choice = Lwt_io.read_line Lwt_io.stdin in
-  let video_mode =
+  let online_mode =
     match choice with
-    | "2" -> true
+    | "1" -> true
     | _ -> false
   in
-  let%lwt () = Lwt_io.printl "Please enter your name here: " in
-  let%lwt name = Lwt_io.read_line Lwt_io.stdin in
-  let user = { name; vid_history = []; genre_counts = Hashtbl.create 10 } in
+  let start_session () =
+    let%lwt () =
+      Lwt_io.printl "Select playback mode: (1) Normal ASCII (2) Video ASCII"
+    in
+    let%lwt choice = Lwt_io.read_line Lwt_io.stdin in
+    let video_mode =
+      match choice with
+      | "2" -> true
+      | _ -> false
+    in
+    let%lwt () = Lwt_io.printl "Please enter your name here: " in
+    let%lwt name = Lwt_io.read_line Lwt_io.stdin in
+    let user = { name; vid_history = []; genre_counts = Hashtbl.create 10 } in
 
-  let videos = load_video_list "videos" in
+    let videos = load_video_list "videos" in
 
-  let ascii_lst =
-    Json_parser.parse_camels "../data/ascii.json"
-    (* List.map
+    let ascii_lst =
+      Json_parser.parse_camels "../data/ascii.json"
+      (* List.map
       (fun x -> { title = "camel"; ascii = x; genre = "camel" })
       Ascii_art.ascii_lst *)
-    (* TODO for ruslan- LOAD from a data directory - *)
+      (* TODO for ruslan- LOAD from a data directory - *)
+    in
+    let rec session_loop () =
+      try%lwt
+        let video = Recommender.recommend user ascii_lst in
+
+        match video with
+        | None ->
+            let%lwt () = Lwt_io.printl "No videos; program ended" in
+            Lwt.return_unit
+        | Some v ->
+            let watch = { video = v; liked = false } in
+
+            (* Play either video or ASCII art *)
+            let%lwt () =
+              (* handles playing videos *)
+              if video_mode then (
+                if !video_index >= List.length videos then
+                  Lwt_io.printl "No more videos :(" >>= fun () ->
+                  Lwt.return_unit
+                else
+                  let video_file = List.nth videos !video_index in
+                  incr video_index;
+                  play_ascii_video video_file)
+              (* handles displaying ascii *)
+                else Lwt_io.printl v.ascii
+            in
+
+            (* Handle user input for this "Tok" *)
+            let rec session_input () =
+              let%lwt input = Lwt_io.read_line Lwt_io.stdin in
+              match String.uppercase_ascii input with
+              | "Q" ->
+                  add_to_history watch user;
+                  Lwt.return_unit
+              | "L" ->
+                  watch.liked <- not watch.liked;
+                  let%lwt () =
+                    Lwt_io.printl
+                      ("You have "
+                      ^ (if watch.liked then "" else "un")
+                      ^ "liked this video")
+                  in
+                  add_to_history watch user;
+                  session_input ()
+              | _ ->
+                  add_to_history watch user;
+                  session_loop ()
+            in
+            session_input ()
+      with _ ->
+        let%lwt () = Lwt_io.printl "An error occurred with this video" in
+        Lwt.return_unit
+    in
+
+    session_loop ()
   in
-  let rec session_loop () =
-    try%lwt
-      let video = Recommender.recommend user ascii_lst in
-
-      match video with
-      | None ->
-          let%lwt () = Lwt_io.printl "No videos; program ended" in
-          Lwt.return_unit
-      | Some v ->
-          let watch = { video = v; liked = false } in
-
-          (* Play either video or ASCII art *)
-          let%lwt () =
-            (* handles playing videos *)
-            if video_mode then (
-              if !video_index >= List.length videos then
-                Lwt_io.printl "No more videos :(" >>= fun () -> Lwt.return_unit
-              else
-                let video_file = List.nth videos !video_index in
-                incr video_index;
-                play_ascii_video video_file)
-            (* handles displaying ascii *)
-              else Lwt_io.printl v.ascii
-          in
-
-          (* Handle user input for this "Tok" *)
-          let rec session_input () =
-            let%lwt input = Lwt_io.read_line Lwt_io.stdin in
-            match String.uppercase_ascii input with
-            | "Q" ->
-                add_to_history watch user;
-                Lwt.return_unit
-            | "L" ->
-                watch.liked <- not watch.liked;
-                let%lwt () =
-                  Lwt_io.printl
-                    ("You have "
-                    ^ (if watch.liked then "" else "un")
-                    ^ "liked this video")
-                in
-                add_to_history watch user;
-                session_input ()
-            | _ ->
-                add_to_history watch user;
-                session_loop ()
-          in
-          session_input ()
-    with _ ->
-      let%lwt () = Lwt_io.printl "An error occurred with this video" in
-      Lwt.return_unit
-  in
-
-  session_loop ()
+  if online_mode then start_session () else Dinotok.run_dino ()
 
 let _ = Lwt_main.run (run ())
